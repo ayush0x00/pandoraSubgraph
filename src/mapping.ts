1d1531429a3d8a148e3c9e2823a0a17010697815
Ayush Tiwari <ayushtiwari5747462@gmail.com>
2021-08-28 11:12:14 +0530

    test

--- src/mapping.ts
+++ src/mapping.ts
@@ -4,14 +4,18 @@ import { TokenTransfer, TokenInfo } from "../generated/schema";
 
 export function handleTransfer(event: Transfer): void {
   let tokenTransfer = new TokenTransfer(
-    event.params.tokenId.toString() + event.address.toHexString()
+    event.transaction.hash.toString() + event.logIndex.toString()
   );
+  tokenTransfer.from = event.params.from.toHexString();
+  tokenTransfer.to = event.params.to.toHexString();
+  tokenTransfer.transferredAt = event.block.timestamp;
+  tokenTransfer.tokenId = event.params.tokenId;
+
   let tokenInfo = TokenInfo.load(
     event.params.tokenId.toString() + event.address.toHexString()
   );
 
   if (!tokenInfo) {
-    log.debug("Creating new token", []);
     tokenInfo = new TokenInfo(
       event.params.tokenId.toString() + event.address.toHexString()
     );
@@ -23,20 +27,19 @@ export function handleTransfer(event: Transfer): void {
     tokenInfo.owner = event.params.to.toHexString();
     tokenInfo.lastTransfer = event.block.timestamp;
     tokenInfo.numberOfTransfers = 1;
+    //tokenInfo.transfers = [event.logIndex.toString()];
     let tokenContract = Contract.bind(event.address);
-    tokenInfo.tokenURI = tokenContract.tokenURI(event.params.tokenId);
+    let result = tokenContract.try_tokenURI(event.params.tokenId);
+    if (result.reverted) tokenInfo.tokenURI = "";
+    else tokenInfo.tokenURI = result.value;
   } else {
-    log.debug("Using existing token...", []);
     tokenInfo.numberOfTransfers = tokenInfo.numberOfTransfers + 1;
     tokenInfo.owner = event.params.to.toHexString();
     tokenInfo.lastTransfer = event.block.timestamp;
     tokenInfo.blockNumber = event.block.timestamp;
+    // tokenInfo.transfers.push(event.logIndex.toString());
   }
-  tokenTransfer.from = event.params.from.toHexString();
-  tokenTransfer.to = event.params.to.toHexString();
-  tokenTransfer.transferredAt = event.block.timestamp;
-  tokenTransfer.tokenId = event.params.tokenId;
-  tokenInfo.transfers =
+  tokenTransfer.tokenInfo =
     event.params.tokenId.toString() + event.address.toHexString();
   tokenTransfer.save();
   tokenInfo.save();
